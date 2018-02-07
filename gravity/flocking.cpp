91102b8d4d2f53dd12d1b9f33d62af789f10a664
Marc Evans <marc.p.evans@gmail.com>
2018-02-06 22:33:40 -0800

    working pull to origin

--- gravity/flocking.cpp
+++ gravity/flocking.cpp
@@ -2,77 +2,128 @@
 using namespace al;
 using namespace std;
 
-float faceRadius = 1.0;
+unsigned numBoids = 10;
+float boidRadius = 1.0;
+double initialRadius = 50;        // initial condition
 float initialSpeed = 10;
 float timeStep = 0.0625;
 double scaleFactor = 0.1;         // resizes the entire scene
+unsigned iterationsPerFrame = 1;
 
-Mesh face;  // global prototype; leave this alone
+// flocking parameters
+float neighborDist = 50;
+
+// when Boids go beyond a certain radius from the origin, they start to turn back
+// with an acceleration proportional to the distance they have gone beyond that radius
+float originPullStartRadius = 100;
+float originPullStrength = 1;
+
+float separationWeight = 1.0;
+float cohesionWeight = 1.0;
+float alignmentWeight = 1.0;
+
+Mesh boid;  // global prototype; leave this alone
 
 // helper function: makes a random vector
 Vec3f r() { return Vec3f(rnd::uniformS(), rnd::uniformS(), rnd::uniformS()); }
 
-struct Face {
-  Vec3f position, velocity, acceleration;
+void limit(Vec3f& v, double maxMagnitude) {
+  if (v.magSqr() > maxMagnitude*maxMagnitude) v.normalize(maxMagnitude);
+}
+
+struct Boid {
   Pose p; // Vec3f + Quatf position+orientation
+  Vec3f position, velocity, acceleration;
+  float maxSpeed = 20.0;
+  float maxAccel = 100.0;
   Color c;
-  Face() {
-    cout << p.uu() << endl;
-    position = Vec3f(0, 0, 0);
-    velocity = Vec3f(0, 0, 0);
-    p.faceToward(Vec3f(0, 0, 1), Vec3f(0, 1, 0));
-    // position = r() * faceRadius;
-    // velocity =
-    //     // this will tend to spin stuff around the y axis
-    //     Vec3f(0, 1, 0).cross(position).normalize(initialSpeed);
+  Boid() {
+    position = r() * initialRadius;
+    velocity =
+        // this will tend to spin stuff around the y axis
+        Vec3f(0, 1, 0).cross(position).normalize(initialSpeed);
+    p.faceToward(velocity);
     c = HSV(rnd::uniform(), 0.7, 1);
   }
   void draw(Graphics& g) {
     g.pushMatrix();
     g.translate(position);
     g.color(c);
-    //g.multMatrix(p.matrix());
-    g.modelView(p.matrix());
-    // g.rotate(p);
-    g.draw(face);
+    g.rotate(p);
+    g.draw(boid);
     g.popMatrix();
   }
-  void step(float dt) {
+  void step(float dt, vector<Boid>& theFlock) {
+     acceleration.zero();
+     acceleration = getSeparationForce(theFlock) * separationWeight + 
+                    getCohesionForce(theFlock) * cohesionWeight + 
+                    getAlignmentForce(theFlock) * alignmentWeight;
+     limit(acceleration, maxAccel);
+     // we add in the origin pull after normalizing so it can't be overcome by the others
+     acceleration += getOriginPullForce();
+     limit(acceleration, maxAccel);
      velocity += acceleration * dt;
-     position += velocity * dt;
+     limit(velocity, maxSpeed);
+     position += velocity * dt; 
+
+     p.faceToward(velocity);
+  }
+  Vec3f getSeparationForce(vector<Boid>& theFlock) {
+    return Vec3f(0, 0, 0);
+  }
+  Vec3f getCohesionForce(vector<Boid>& theFlock) {
+    return Vec3f(0, 0, 0);
+  }
+  Vec3f getAlignmentForce(vector<Boid>& theFlock) {
+    return Vec3f(0, 0, 0);
+  }
+  Vec3f getOriginPullForce() {
+    float distFromOrigin = position.mag();
+    // cout << distFromOrigin << endl;
+    if(distFromOrigin > originPullStartRadius) {
+      return -position/distFromOrigin;
+    } else { return Vec3f(0, 0, 0); }
   }
 };
 
-struct MyApp : App {
+struct FlockingFaces : App {
   Material material;
   Light light;
-  bool simulate = true;
+  bool simulate = true, runOneFrame = false;
 
-  Face f;
+  vector<Boid> boids;
 
-  MyApp() {
+  FlockingFaces() {
 
     light.pos(5, 5, 5);              // place the light
     nav().pos(0, 0, 30*scaleFactor);             // place the viewer
     lens().far(400*scaleFactor);                 // set the far clipping plane
     background(Color(0.07));
 
+    boids.resize(numBoids);
     initWindow();
     initAudio();
   }
 
   void onAnimate(double dt) {
-    if (!simulate)
+    if (!simulate && !runOneFrame)
       // skip the rest of this function
       return;
-    f.step(timeStep);
+    // I made pressing 5 when the simulation is stopped advance the simulation by a single frame
+    // It was useful in debugging the application.
+    runOneFrame = false;
+    for (unsigned k = 0; k < iterationsPerFrame; ++k) {
+      for (auto& b : boids)
+        b.step(timeStep / iterationsPerFrame, boids);
+    }
   }
 
   void onDraw(Graphics& g) {
     material();
     light();
     g.scale(scaleFactor);
-    f.draw(g);
+    for (auto& b : boids)
+      b.draw(g);
   }
 
   void onSound(AudioIO& io) {
@@ -101,22 +152,26 @@ struct MyApp : App {
         // pause the simulation
         simulate = !simulate;
         break;
+      case '5':
+        // advance the simulation by a single frame (when the simulation is paused)
+        runOneFrame = true;
+        break;
     }
   }
 };
 
-void constructFaceMesh() {
-  addSphere(face, faceRadius);
-  face.translate(Vec3f(faceRadius*2.5, 0, 0));
-  addSphere(face, faceRadius);
-  face.translate(Vec3f(-faceRadius*1.25, 0, 0));
-  face.scale(0.4, 0.4, 0.4);
-  face.translate(Vec3f(0, faceRadius*0.4, -faceRadius*0.7));
-  addSphere(face, faceRadius);
-  face.translate(Vec3f(0, 0, 0.8));
-  addCone(face, faceRadius/4, Vec3f(0, 0, -1.0));
-  face.translate(Vec3f(0, 0, -0.8));
-  face.generateNormals();
+void constructBoidMesh() {
+  addSphere(boid, boidRadius);
+  boid.translate(Vec3f(boidRadius*2.5, 0, 0));
+  addSphere(boid, boidRadius);
+  boid.translate(Vec3f(-boidRadius*1.25, 0, 0));
+  boid.scale(0.4, 0.4, 0.4);
+  boid.translate(Vec3f(0, boidRadius*0.4, -boidRadius*0.7));
+  addSphere(boid, boidRadius);
+  boid.translate(Vec3f(0, 0, 0.8));
+  addCone(boid, boidRadius/4, Vec3f(0, 0, -1.0));
+  boid.translate(Vec3f(0, 0, -0.8));
+  boid.generateNormals();
 }
 
-int main() { constructFaceMesh(); MyApp().start(); }
+int main() { constructBoidMesh(); FlockingFaces().start(); }
